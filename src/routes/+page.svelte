05b98968faca374615fe7479b293fbaa03d45023
Marc-André Tremblay <1619947+marctrem@users.noreply.github.com>
2026-09-20 21:01:41 -0400

    Remove trailing period from tagline

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+	import { resolve } from '$app/paths';
+</script>
+
 <svelte:head>
 	<title>Marc Tremblay</title>
 	<meta name="description" content="Personal homepage of Marc Tremblay." />
@@ -5,9 +9,10 @@
 
 <main>
 	<h1>Marc Tremblay</h1>
-	<p class="tagline">Personal homepage.</p>
+	<p class="tagline">Personal homepage</p>
 	<nav>
 		<a href="https://github.com/marctrem">GitHub</a>
+		<a href={resolve('/titanic-fly-classifier')}>Titanic fly classifier</a>
 		<span class="email">m&nbsp;[@]&nbsp;rctrem.ca</span>
 	</nav>
 </main>
